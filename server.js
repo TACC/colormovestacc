1d8c053aa23654d19ac072707a497dd39c285e23
John Gentle <taoteg@gmail.com>
2022-04-20 10:19:37 -0500

    Made app route configurable in server.js.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const serveStatic = require('serve-static');
 // Declare Constants
 const PORT = 8888;
 const HOST = '0.0.0.0';
+const APP_ROUTE = '/colormoves/app';
 
 // Define App
 const app = express();
@@ -16,7 +17,7 @@ app.use(serveStatic(path.join(__dirname, '.')));
 app.use(serveStatic(path.join(__dirname, 'lib')));
 
 // App Route Responses.
-app.get('/colormoves', (req, res) => {
+app.get(`${APP_ROUTE}`, (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
@@ -24,4 +25,4 @@ app.get('/colormoves', (req, res) => {
 app.listen(PORT, HOST);
 
 // Logging.
-// console.log(`Running on http://${HOST}:${PORT}`
+console.log(`Running on http://${HOST}:${PORT}/${APP_ROUTE}`);
